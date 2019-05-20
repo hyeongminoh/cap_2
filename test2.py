457d1d4345f756c4be35741f472b27539fcbe1ee
오형민 <mihn0621@gmail.com>
2019-05-20 22:04:46 +0900

    a

--- test2.py
+++ test2.py
@@ -23,11 +23,11 @@ dataloader = torch.utils.data.DataLoader( dataset = dataset, batch_size=1, shuff
 for X, y in iter(dataloader):
 	#print(X.shape)
 	if isinstance(y['annotation']['object'],list) == True:
-		#if y['annotation']['object'][0] == 'car' :
-		for i in y['annotation']['object']:
+		if y['annotation']['object'][0] == 'car' :
+			for i in y['annotation']['object']:
 			print('l' , i)
 	else:
-		#if y['annotation']['object']['name'] == 'car' :
+		if y['annotation']['object']['name'] == 'car' :
 		for i in y['annotation']['object'].keys():
 			print('k', i, type(y['annotation']['object'][i]), y['annotation']['object'][i])
 
