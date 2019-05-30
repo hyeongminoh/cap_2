3a58e7da7c527d7159093e7a31f8cfcf4ee24e7c
오형민 <mihn0621@gmail.com>
2019-05-30 21:59:20 +0900

    1차 완료

--- voc20_squeezenet_1.py
+++ voc20_squeezenet_1.py
@@ -21,12 +21,12 @@ transform = transforms.Compose([transforms.Resize((240,240)),
 trainset = torchvision.datasets.VOCDetection("/data/datasets", year='2012', image_set='trainval', 
                                 download=True, transform=transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=8, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.VOCDetection("/data/datasets", year='2012', image_set='trainval', 
                                 download=True, transform=transform)
 
-testloader = torch.utils.data.DataLoader(testset, batch_size=8, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
 
 # Model
