fcf6fbf72053490208bed26d643a605c37255c07
오형민 <mihn0621@gmail.com>
2019-05-30 22:03:51 +0900

    1차 완료

--- voc20_squeezenet_1.py
+++ voc20_squeezenet_1.py
@@ -16,7 +16,7 @@ print('===> Loading Data...')
 
 transform = transforms.Compose([transforms.Resize((240,240)),
                                 transforms.ToTensor(),
-                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+                                transforms.Normalize((0.1307,), (0.3081,))])
 
 trainset = torchvision.datasets.VOCDetection("/data/datasets", year='2012', image_set='trainval', 
                                 download=True, transform=transform)
