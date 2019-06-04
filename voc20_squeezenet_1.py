4e403d63ded9653effa5a50393e3dc626af9acd0
오형민 <mihn0621@gmail.com>
2019-06-04 21:55:40 +0900

    gpu지정

--- voc20_squeezenet_1.py
+++ voc20_squeezenet_1.py
@@ -37,7 +37,7 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False, n
 print('===> Building Model - 1channel_squeezenet1_0 - ...')
 os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 2, 3'
 net = models.squeezenet1_0(pretrained=False)
-net = nn.DataParallel(net, output_device=3)
+#net = nn.DataParallel(net, output_device=3)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
