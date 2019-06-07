ef724d00d4fe616dc7521f6a22843d5dc76f25d0
오형민 <mihn0621@gmail.com>
2019-06-07 21:22:55 +0900

    1channel modified

--- 1channel_test.py
+++ 1channel_test.py
@@ -127,7 +127,7 @@ GPU 사용하기
 print('\n===> Training Start')
 start_vect=time.time()
 
-device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(device)
 
 net.to(device)
